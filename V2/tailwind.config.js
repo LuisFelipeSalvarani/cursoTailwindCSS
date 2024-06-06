40924a135e824181e55bad1d16050cd7c8cb01b3
Luis Felipe Salvarani <felipe_salvarani@hotmail.com>
2024-06-06 16:55:20 -0300

    Novas cores

--- V2/tailwind.config.js
+++ V2/tailwind.config.js
@@ -1,8 +1,14 @@
 /** @type {import('tailwindcss').Config} */
+
+const colors = require('tailwindcss/colors')
+
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      amber: colors.amber,
+      rose: colors.rose
+    },
   },
   plugins: [],
 }
